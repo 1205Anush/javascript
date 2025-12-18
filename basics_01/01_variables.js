d9ef993bd10a2ab116bc448422ed6264c88d4484
1205Anush <141100071+1205Anush@users.noreply.github.com>
2025-12-18 09:58:23 +0530

    data types

---
+++ basics_01/01_variables.js
@@ -0,0 +1,33 @@
+// data types in js
+// number: represents both integer and floating point numbers
+let a =100
+let r = 1.23;
+
+//string:sequence of characters. IMMUTABLE;
+let str = "anush sonkusale";
+//console.log(str);
+str = "hello world";
+console.log(str);
+
+//boolean : LOGICAL VALUES; TRUE OR FALSE
+let isActive = true;
+let isTrue = false;
+
+console.log(0.1 + 0.2);
+
+
+//symbol: UNIQUE PRIMITIVE IDENTIFIER. EVEN WHEN THE APPEAR SAME, THEUY ARE NOT
+/*const a = Symbol("id");
+const b = Symbol("id");
+
+console.log(a === b); // false
+
+Symbols create collision-proof keys
+*/
+
+//OBJECTS: An object is a collection of key–value pairs where keys 
+// are strings or symbols and values can be anything.
+/*Stored on the heap
+ Variables hold references, not values
+ Mutable by default*/
+// keys are string by default
